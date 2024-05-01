77a86db0f29abf3268b1cb2257e14d3f3920ea30
Jordan Burger <jordanburger22@gmail.com>
2024-05-01 10:08:12 -0600

    init

---
+++ client/src/components/Issue.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import moment from 'moment'
+
+export default function Issue(props) {
+  const { title, description, _id, username, createdAt } = props
+  
+  const timeStamp = moment(createdAt).fromNow()
+  return (
+    <div className="todo">
+
+      <h3>Issue posted by: {username}</h3>
+      <h1>Title: {title}</h1>
+      <h3>Description: {description}</h3>
+      <p>{timeStamp}</p>
+      <div>
+        <p>Upvotes: 0</p>
+        <button>Upvote</button>
+      </div>
+
+      <div>
+        <p>Downvotes: 0</p>
+        <button>Downvote</button>
+      </div>
+    </div>
+  )
+}
